72a8a6afdfa0d129998020a248b7a332e8a56b7e
grasypatel <gracepatel91@gmail.com>
2025-08-18 14:34:03 +0530

    error

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  origin:"https://checkbeforegohelp.onrender.com",
   credentials: true,
 };
 
